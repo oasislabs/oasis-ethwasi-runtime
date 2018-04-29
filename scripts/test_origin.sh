24dd5b8a89c2b0377d8712e05a8fb3fb8a26f2a8
Noah Johnson <noahj@oasislabs.com>
2018-04-28 17:12:13 -0700

    fix origin script

--- scripts/test_origin.sh
+++ scripts/test_origin.sh
@@ -1,11 +1,32 @@
 echo 'Creating listing contract' && \
-./request.sh payloads/origin/create_listing_contract.json && \
+./request.sh payloads/origin/1_*.json && \
 
-echo 'Get listing count' && \
-./request.sh payloads/origin/listings_length.json && \
+sleep 1s
+echo ''
+echo 'Creating counter contract (to increase nonce)' && \
+./request.sh payloads/counter/create_counter_contract.json && \
 
-echo 'Creating new listing (raw)' && \
-./request.sh payloads/origin/create_new_listing_raw_transaction.json && \
+sleep 2s
+echo ''
+echo 'Get owner' && \
+./request.sh payloads/origin/2_*.json
 
-echo 'Get listing ount' && \
-./request.sh payloads/origin/listings_length.json
+echo ''
+echo 'Get listings length' && \
+./request.sh payloads/origin/3_*.json
+
+echo ''
+echo 'Estimating gas for creating new listing' && \
+./request.sh payloads/origin/4_*.json
+
+### Note: this raw transaction uses nonce = 2. If you reorder the transactions in this
+### script, you must ensure the 0x711...2b account has current nonce value 2 or Sputnik
+### will reject this transaction.
+echo ''
+echo 'Creating new listing' && \
+./request.sh payloads/origin/5_*.json
+
+echo ''
+echo 'Get listings length' && \
+./request.sh payloads/origin/3_*.json
+
