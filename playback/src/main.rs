1c67e1b29177150fa67f37923c2bc5f21f933608
Warren He <w@oasislabs.com>
2018-05-09 15:45:18 -0700

    playback: provide start state, custom args

--- playback/src/main.rs
+++ playback/src/main.rs
@@ -2,10 +2,6 @@
 extern crate clap;
 use clap::App;
 use clap::Arg;
-use clap::crate_authors;
-use clap::crate_description;
-use clap::crate_name;
-use clap::crate_version;
 use clap::value_t;
 extern crate filebuffer;
 extern crate futures;
@@ -13,10 +9,12 @@ use futures::future::Future;
 extern crate grpcio;
 extern crate hex;
 extern crate rlp;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
 
 extern crate client_utils;
 use client_utils::contract_client;
-use client_utils::default_app;
 use client_utils::default_backend;
 extern crate ekiden_contract_client;
 use ekiden_contract_client::create_contract_client;
@@ -30,14 +28,74 @@ with_api! {
     create_contract_client!(evm, evm_api, api);
 }
 
+#[derive(Deserialize)]
+struct ExportedAccount {
+    balance: String,
+    nonce: String,
+}
+#[derive(Deserialize)]
+struct ExportedState {
+    state: std::collections::HashMap<String, ExportedAccount>,
+}
+
 fn main() {
     let seed = ekiden_core::bytes::B256::random();
     let seed_input = ekiden_core::untrusted::Input::from(&seed);
     let key_pair = ekiden_core::ring::signature::Ed25519KeyPair::from_seed_unchecked(seed_input).unwrap();
     let signer = std::sync::Arc::new(ekiden_core::signature::InMemorySigner::new(key_pair));
-    let mut client = contract_client!(signer, evm);
+    let args = App::new("playback client")
+        .arg(
+            Arg::with_name("exported_state")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("exported_blocks")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(Arg::with_name("host")
+             .long("host")
+             .short("h")
+             .takes_value(true)
+             .default_value("127.0.0.1")
+             .display_order(1))
+        .arg(Arg::with_name("port")
+             .long("port")
+             .short("p")
+             .takes_value(true)
+             .default_value("9001")
+             .display_order(2))
+        .arg(Arg::with_name("nodes")
+             .long("nodes")
+             .help("A list of comma-separated compute node addresses (e.g. host1:9001,host2:9004)")
+             .takes_value(true))
+        .arg(Arg::with_name("mr-enclave")
+             .long("mr-enclave")
+             .value_name("MRENCLAVE")
+             .help("MRENCLAVE in hex format")
+             .takes_value(true)
+             .required(true)
+             .display_order(3))
+        .get_matches();
+    let mut client = contract_client!(signer, evm, args);
 
-    let blocks_path = std::env::args().nth(1).expect("Usage: playback EXPORTED_BLOCKS");
+    let state_path = args.value_of("exported_state").unwrap();
+    let state: ExportedState = serde_json::from_reader(std::fs::File::open(state_path).unwrap()).unwrap();
+    let res = client.init_genesis_block({
+        let mut req = evm_api::InitStateRequest::new();
+        for (addr, account) in state.state {
+            let mut account_state = evm_api::AccountState::new();
+            account_state.set_nonce(account.nonce);
+            account_state.set_address(addr);
+            account_state.set_balance(account.balance);
+            req.accounts.push(account_state);
+        }
+        req
+    }).wait().unwrap();
+    println!("init_genesis_block: {:?}", res);
+
+    let blocks_path = args.value_of("exported_blocks").unwrap();
     // Blocks are written one after another into the exported blocks file.
     // https://github.com/paritytech/parity/blob/v1.9.7/parity/blockchain.rs#L595
     let blocks_raw = filebuffer::FileBuffer::open(blocks_path).unwrap();
@@ -59,7 +117,7 @@ fn main() {
                 req.set_data(hex::encode(transaction_raw));
                 req
             }).wait().unwrap();
-            println!("{:?}", res);
+            println!("execute_raw_transaction: {:?}", res);
         }
     }
 }
