dd915b21433fafeae4b2c8908944565a1579372f
Warren He <w@oasislabs.com>
2018-05-15 01:02:19 +0000

    deployment: switch to debug builds

--- docker/deployment/build-images-inner.sh
+++ docker/deployment/build-images-inner.sh
@@ -15,17 +15,17 @@ EOF
 fi
 
 # Build all Ekiden binaries and resources.
-CARGO_TARGET_DIR=target cargo install --force --git https://github.com/oasislabs/ekiden --branch master ekiden-tools
-cargo ekiden build-contract --git https://github.com/oasislabs/ekiden --branch master --output target/contract --target-dir target --release ekiden-key-manager
-cargo ekiden build-contract --release --output-identity
-(cd client && CARGO_BUILD_TARGET_DIR=../target cargo build --release)
+CARGO_TARGET_DIR=target cargo install --force --git https://github.com/oasislabs/ekiden --branch master --debug ekiden-tools
+cargo ekiden build-contract --git https://github.com/oasislabs/ekiden --branch master --output target/contract --target-dir target ekiden-key-manager
+cargo ekiden build-contract --output-identity
+(cd client && CARGO_BUILD_TARGET_DIR=../target cargo build)
 
 # Package all binaries and resources.
 mkdir -p target/docker-deployment/context/bin target/docker-deployment/context/lib target/docker-deployment/context/res
 ln target/contract/ekiden-key-manager.so target/docker-deployment/context/lib/evm-key-manager.so
 ln target/contract/evm.so target/docker-deployment/context/lib
 ln target/contract/evm.mrenclave target/docker-deployment/context/res
-ln target/release/web3-client target/docker-deployment/context/bin
+ln target/debug/web3-client target/docker-deployment/context/bin
 ln docker/deployment/Dockerfile target/docker-deployment/context/Dockerfile
 tar cvzhf target/docker-deployment/context.tar.gz -C target/docker-deployment/context .
 rm -rf target/docker-deployment/context
