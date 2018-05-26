421735b4445ea73e480dc4f89f8fe26d76278a57
Noah Johnson <noahj@oasislabs.com>
2018-05-25 17:02:18 -0700

    implement eth_blockNumber

--- client/src/rpc/serves.rs
+++ client/src/rpc/serves.rs
@@ -116,13 +116,9 @@ impl EthereumRPC for MinerEthereumRPC {
     }
 
     fn block_number(&self) -> Result<Hex<usize>, Error> {
-        /*
-        println!("\n*** block_number");
-        let state = self.state.lock().unwrap();
-
-        Ok(Hex(state.block_height()))
-        */
-        Err(Error::TODO)
+        let block_height = self.client.get_block_height(false).wait().unwrap();
+        let result = U256::from_str(&block_height)?.as_usize();
+        Ok(Hex(result))
     }
 
     fn balance(&self, address: Hex<Address>, block: Trailing<String>) -> Result<Hex<U256>, Error> {
