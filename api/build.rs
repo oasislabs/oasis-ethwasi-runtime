53c3aa3d894734819cf286ffd4842f0724b4ef83
Peter Gilbert <pgilbert@oasislabs.com>
2018-06-12 21:02:29 +0000

    Fix the build

--- api/build.rs
+++ api/build.rs
@@ -1,7 +1,6 @@
 extern crate ekiden_tools;
 
 fn main() {
-    // not using protobufs
-    //ekiden_tools::generate_mod("src/generated", &["api"]);
+    ekiden_tools::generate_mod("src/generated", &["api"]);
     ekiden_tools::build_api();
 }
