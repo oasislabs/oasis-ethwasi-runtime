533d43575708f25e616c944e66446e74c00c4429
Peter Gilbert <pgilbert@oasislabs.com>
2018-06-21 01:23:12 +0000

    Fix eth_getBlockByNumber: U256::from_str args should not be prefixed by 0x

--- client/src/rpc/util.rs
+++ client/src/rpc/util.rs
@@ -40,7 +40,7 @@ pub fn to_rpc_block(block: Block, full_transactions: bool) -> Result<RPCBlock, E
 
     size: Hex(0),
     // FIXME: gas_limits that are too high overflow metamask, so pick an arbitrary not-too-large number
-    gas_limit: Hex(U256::from_str("0x10000000000000").unwrap()),
+    gas_limit: Hex(U256::from_str("10000000000000").unwrap()),
     gas_used: Hex(U256::zero()),
     timestamp: Hex(0),
     transactions: if full_transactions {
