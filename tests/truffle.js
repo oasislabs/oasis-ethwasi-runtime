9891fe0f1a677b13082bec22565171e8cfb8f2fd
Peter Gilbert <pgilbert@oasislabs.com>
2018-07-10 21:03:54 +0000

    Use HDWallet for truffle tests

--- tests/truffle.js
+++ tests/truffle.js
@@ -1,16 +1,16 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+
 module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 8545,
-      from: "0x7110316b618d20d0c44728ac2a3d683536ea682b",
-      network_id: "*"
-    },
-    testnet: {
-      host: "oasiscloud.io",
-      port: 8545,
-      from: "0x7110316b618d20d0c44728ac2a3d683536ea682b",
-      network_id: "*"
-    },
+      provider: function () {
+        return new HDWalletProvider(mnemonic, "http://localhost:8545/");
+      },
+      network_id: "*",
+      gasPrice: 0
+    }
   }
 };
