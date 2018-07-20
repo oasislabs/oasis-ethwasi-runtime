a8ad596e35cd288bd36637772af796cab2de2ab5
David Yan <david.yan@berkeley.edu>
2018-07-20 22:24:45 +0000

    Modify storage to implement store_bytes

--- src/storage.rs
+++ src/storage.rs
@@ -39,4 +39,11 @@ impl Storage for StorageImpl {
             Err(err) => Err(Error::Storage(err.description().to_string())),
         }
     }
+
+    fn store_bytes(&mut self, key: H256, bytes: &[u8]) {
+        let mut db = DatabaseHandle::instance();
+        let mut key_bytes = Vec::new();
+        key.copy_to(&mut key_bytes);
+        db.insert(&mut key_bytes, bytes);
+    }
 }
