1698e810b4e94dc7541837d668bf4ff3aadedcd8
David Yan <david.yan@berkeley.edu>
2018-07-20 21:32:30 +0000

    Changed request_bytes key type to H256 and removed unnecessary dependency

--- src/storage.rs
+++ src/storage.rs
@@ -1,11 +1,14 @@
-use ekiden_db_trusted::{handle::DatabaseHandle, Database};
+use ekiden_trusted::db::{handle::DatabaseHandle, Database};
 use ethcore::storage::Storage;
+use ethereum_types::H256;
 
 pub struct StorageImpl {}
 
 impl Storage for StorageImpl {
-    fn request_bytes(&mut self, key: &[u8]) -> Option<Vec<u8>> {
+    fn request_bytes(&mut self, key: H256) -> Option<Vec<u8>> {
         let mut db = DatabaseHandle::instance();
-        db.get(key)
+        let mut key_bytes = Vec::new();
+        key.copy_to(&mut key_bytes);
+        db.get(&key_bytes)
     }
 }
