2dabb74af0cae5a54b42474483bedd3caa552135
David Yan <david.yan@berkeley.edu>
2018-07-24 21:42:30 +0000

    Bypass DatabaseHandle to use StorageBackend directly

--- src/storage.rs
+++ src/storage.rs
@@ -1,21 +1,46 @@
-use ekiden_trusted::db::{handle::DatabaseHandle, Database};
+use ekiden_common::bytes::H256 as ekiden_H256;
+use ekiden_core::futures::Future;
+use ekiden_storage_base::StorageBackend;
+#[cfg(not(target_env = "sgx"))]
+use ekiden_storage_dummy::DummyStorageBackend;
+#[cfg(target_env = "sgx")]
+use ekiden_trusted::db::untrusted::UntrustedStorageBackend;
 use ethcore::storage::Storage;
 use ethereum_types::H256;
 
+use std::str::FromStr;
+use std::sync::Arc;
+
 pub struct StorageImpl {}
 
+lazy_static! {
+    static ref BACKEND: Arc<StorageBackend> = {
+        #[cfg(not(target_env = "sgx"))]
+        let backend = Arc::new(DummyStorageBackend::new());
+        #[cfg(target_env = "sgx")]
+        let backend = Arc::new(UntrustedStorageBackend::new());
+        backend
+    };
+}
+
 impl Storage for StorageImpl {
-    fn request_bytes(&mut self, key: H256) -> Option<Vec<u8>> {
-        let mut db = DatabaseHandle::instance();
-        let mut key_bytes: Vec<u8> = Vec::with_capacity(32);
-        key.copy_to(&mut key_bytes);
-        db.get(&key_bytes)
+    fn request_bytes(&mut self, key: H256) -> Result<Vec<u8>, String> {
+        let backend = BACKEND.clone();
+        let result = backend
+            .get(ekiden_H256::from_str(&format!("{:x}", key)).unwrap())
+            .wait();
+        match result {
+            Ok(value) => Ok(value),
+            Err(error) => Err(error.description().to_string()),
+        }
     }
 
-    fn store_bytes(&mut self, key: H256, bytes: &[u8]) {
-        let mut db = DatabaseHandle::instance();
-        let mut key_bytes: Vec<u8> = Vec::with_capacity(32);
-        key.copy_to(&mut key_bytes);
-        db.insert(&mut key_bytes, bytes);
+    fn store_bytes(&mut self, key: H256, bytes: &[u8]) -> Result<(), String> {
+        let backend = BACKEND.clone();
+        let result = backend.insert(bytes.to_vec(), <u64>::max_value()).wait();
+        match result {
+            Ok(_) => Ok(()),
+            Err(error) => Err(error.description().to_string()),
+        }
     }
 }
