4705783afe8d624b1a2ed4ed67cc99f6c9c8369d
Peter Gilbert <pgilbert@oasislabs.com>
2018-09-05 21:57:14 +0000

    Use go dummy node in gateway.sh

--- scripts/gateway.sh
+++ scripts/gateway.sh
@@ -2,13 +2,23 @@
 
 WORKDIR=${1:-$(pwd)}
 
-run_dummy_node_default() {
-    echo "Starting dummy node."
+run_dummy_node() {
+    local datadir=/tmp/ekiden-dummy-data
+    rm -rf ${datadir}
 
-    ekiden-node-dummy \
-	--entity-ethereum-address 0000000000000000000000000000000000000000 \
-	--time-source-notifier mockrpc \
-        --storage-backend dummy \
+    echo "Starting Go dummy node."
+
+    ekiden \
+        --log.level debug \
+        --grpc.port 42261 \
+        --epochtime.backend tendermint \
+        --epochtime.tendermint.interval 30 \
+        --beacon.backend tendermint \
+        --storage.backend memory \
+        --scheduler.backend trivial \
+        --registry.backend tendermint \
+        --roothash.backend tendermint \
+        --datadir ${datadir} \
         &> dummy.log &
 }
 
@@ -48,18 +58,15 @@ run_test() {
     run_compute_node 1
     sleep 1
     run_compute_node 2
-
-    # Advance epoch to elect a new committee.
-    echo "Advancing epoch."
-    sleep 2
-    ekiden-node-dummy-controller set-epoch --epoch 1
     sleep 2
 
     # Run the client. We run the client first so that we test whether it waits for the
     # committee to be elected and connects to the leader.
     echo "Starting web3 gateway."
     gateway/target/debug/gateway \
-        --storage-backend remote \
+        --storage-backend multilayer \
+        --storage-multilayer-local-storage-base /tmp/ekiden-storage-persistent-gateway \
+        --storage-multilayer-bottom-backend remote \
         --mr-enclave $(cat $WORKDIR/target/enclave/runtime-ethereum.mrenclave) \
         --threads 100 &> gateway.log &
     gateway_pid=$!
@@ -72,4 +79,4 @@ run_test() {
     wait || true
 }
 
-run_test run_dummy_node_default
+run_test run_dummy_node
