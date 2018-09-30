e9e10908cb93a70c3d1e11a054cfbd6d073b2afb
Armani Ferrante <armaniferrante@gmail.com>
2018-09-30 21:58:12 +0000

    Web3(c) V0.5 shared confidentiality utilities

---
+++ common/src/lib.rs
@@ -0,0 +1,8 @@
+//! Common utilities shared between runtime-ethereum and the gateway.
+
+extern crate ekiden_common;
+extern crate ekiden_core;
+extern crate ekiden_keymanager_common;
+extern crate sodalite;
+
+pub mod confidential;
