94fc0f154c3be72f91cde4d946c98174001c236e
Peter Gilbert <pgilbert@oasislabs.com>
2018-10-01 02:27:09 +0000

    Fund account used for truffle tests and txclient

--- docker/transactionclient/src/web3.js
+++ docker/transactionclient/src/web3.js
@@ -15,7 +15,7 @@ var makeTxn = function (client) {
         client.eth.sendTransaction({
             from: "1cca28600d7491365520b31b466f88647b9839ec",
             gas: 100000,
-            gasPrice: 0,
+            gasPrice: 20000000000,
             to: "1cca28600d7491365520b31b466f88647b9839ec"
         }, function (err, txn) {
             if (err) {
