718b4df20725c44dd7f773972a9a4b2edeea298c
Will <willscott@gmail.com>
2018-10-02 16:45:02 -0700

    Fix error in tx client (#237)

--- docker/transactionclient/src/web3.js
+++ docker/transactionclient/src/web3.js
@@ -4,27 +4,27 @@
  * send 0-value transactions to itself at a fixed interval.
  */
 
-var pause = async function (timeout) {
-    return new Promise(function (resolve, _reject) {
-        setTimeout(resolve, timeout);
-    });
+async function pause (timeout) {
+  return new Promise(function (resolve, _reject) {
+    setTimeout(resolve, timeout);
+  });
 };
 
-var makeTxn = function (client) {
-    return new Promise(function (resolve, reject) {
-        client.eth.sendTransaction({
-            from: "1cca28600d7491365520b31b466f88647b9839ec",
-            gas: 100000,
-            gasPrice: 1000000000,
-            to: "1cca28600d7491365520b31b466f88647b9839ec"
-        }, function (err, txn) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(txn);
-            }
-        });
+function makeTxn (client) {
+  return new Promise(function (resolve, reject) {
+    client.eth.sendTransaction({
+      from: "1cca28600d7491365520b31b466f88647b9839ec",
+      gas: 100000,
+      gasPrice: 1000000000,
+      to: "1cca28600d7491365520b31b466f88647b9839ec"
+    }, function (err, txn) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(txn);
+      }
     });
+  });
 };
 
 // Only run if directly executed.
@@ -33,73 +33,73 @@ if (require.main === module) {
     let web3 = require('web3');
     const client = require('prom-client');
     const txncount = new client.Counter({
-        name: 'txncount',
-        help: 'Number of web3 transactions made'
+      name: 'txncount',
+      help: 'Number of web3 transactions made'
     });
     const txnerrors = new client.Counter({
-        name: 'txnerrors',
-        help: 'Number of errored web3 transactions'
+      name: 'txnerrors',
+      help: 'Number of errored web3 transactions'
     });
     const txnlatency = new client.Summary({
-        name: 'txnlatencysummary',
-        help: 'Latency summary of web3 transactions',
+      name: 'txnlatencysummary',
+      help: 'Latency summary of web3 transactions',
     });
 
     let mnemonic = 'patient oppose cotton portion chair gentle jelly dice supply salmon blast priority';
 
     if (process.argv.length < 4) {
-        console.warn("Usage: web3.js <provider> <interval-ms> [pushgateway address] [push job prefix] [push instance label]");
-        process.exit(0);
+      console.warn("Usage: web3.js <provider> <interval-ms> [pushgateway address] [push job prefix] [push instance label]");
+      process.exit(0);
     }
     let gateway = null;
     let pushJobPrefix = "";
     let pushGroupings = {};
     if (process.argv.length < 5) {
-        const http = require('http');
-        let server = http.createServer((req, res) => {
-            res.end(client.register.metrics());
-        });
-        server.listen(3000);
+      const http = require('http');
+      let server = http.createServer((req, res) => {
+        res.end(client.register.metrics());
+      });
+      server.listen(3000);
     } else if (process.argv.length == 7) {
-        gateway = new client.Pushgateway(process.argv[4]);
-        pushJobPrefix = process.argv[5];
-        pushGroupings['instance'] = process.argv[6];
+      gateway = new client.Pushgateway(process.argv[4]);
+      pushJobPrefix = process.argv[5];
+      pushGroupings['instance'] = process.argv[6];
     } else {
-        console.warn("Usage: web3.js <provider> <interval-ms> [pushgateway address] [push job prefix] [push instance label]");
-        process.exit(0);
+      console.warn("Usage: web3.js <provider> <interval-ms> [pushgateway address] [push job prefix] [push instance label]");
+      process.exit(0);
     }
 
-    let next_txn = async function (client) {
-        try {
-            await makeTxn(client);
-        } catch (e) {
-            console.warn("Transaction failed.", e);
-            txnerrors.inc();
-        }
+    async function nextTxn (client) {
+      try {
+        await makeTxn(client);
+      } catch (e) {
+        console.warn("Transaction failed.", e);
+        txnerrors.inc();
+      }
     }
 
-    let run = async function () {
-        let client = new web3();
-        let hprovider = new client.providers.HttpProvider(process.argv[2], 1000 * 60);
-        let provider = new HDWalletProvider(mnemonic, hprovider);
-        await client.setProvider(provider);
-        let in_progress = new Map();
-        while (true) {
-            await pause(process.argv[3]);
+    async function run () {
+      let client = new web3();
+      let hprovider = new client.providers.HttpProvider(process.argv[2], 1000 * 60);
+      let provider = new HDWalletProvider(mnemonic, hprovider);
+      await client.setProvider(provider);
+      let in_progress = new Map();
+      while (true) {
+        await pause(process.argv[3]);
 
-            let end = txnlatency.startTimer();
-            let next_txn = nextTxn(client);
-            in_progress.set(next_txn, end);
-            next_txn.then(() => {
-                in_progress.get(next_txn)();
-                in_progress.delete(next_txn);
-                txncount.inc();
-            });
+        let end = txnlatency.startTimer();
+        let next_txn = nextTxn(client);
+        in_progress.set(next_txn, end);
+        next_txn.then(() => {
+          in_progress.get(next_txn)();
+          in_progress.delete(next_txn);
+          txncount.inc();
+        });
 
-            if (gateway != null) {
-                gateway.pushAdd({ jobName: `${pushJobPrefix}-web3-txn`, groupings: pushGroupings }, () => { });
-            }
+        if (gateway != null) {
+          gateway.pushAdd({ jobName: `${pushJobPrefix}-web3-txn`, groupings: pushGroupings }, () => { });
         }
+      }
     };
 
     run();
