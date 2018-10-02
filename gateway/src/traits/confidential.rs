d4b069ba56a63077ea2831d50100619c009cf4ec
Armani Ferrante <armaniferrante@gmail.com>
2018-10-02 20:00:52 +0000

    Add comment to confidential_sendRawTransaction

--- gateway/src/traits/confidential.rs
+++ gateway/src/traits/confidential.rs
@@ -22,6 +22,12 @@ build_rpc_trait! {
             Trailing<BlockNumber>
         ) -> BoxFuture<Bytes>;
         /// Executes a raw transaction, where the data field is encrypted.
+        /// Using confidential_sendRawTransaction insted of eth_sendRawTransaction
+        /// because, for v0.5, we need a way to distinguish between encrypted and
+        /// non-encrypted transactions. Otherwise, tools such as truffle will break.
+        /// Once we have a scheme for encoding encrypted raw transactions, e.g.,
+        /// by prefixing the bytes here with a unique set of bytes, then we should
+        /// merge confidential_sendRawTransaction into eth_sendRawTransaction.
         #[rpc(name = "confidential_sendRawTransaction")]
         fn send_raw_transaction(&self, Bytes) -> Result<H256>;
     }
