ca928fb12a930e66ab09bb204441aa768d96bac5
David Yan <david.yan@berkeley.edu>
2018-10-08 20:52:29 +0000

    Restore test-e2e.sh

--- scripts/test-e2e.sh
+++ scripts/test-e2e.sh
@@ -66,7 +66,72 @@ run_gateway() {
         --prometheus-mode pull &> gateway${id}.log &
 }
 
+run_dummy_node_go_tm() {
+    local datadir=/tmp/ekiden-dummy-data
+    rm -rf ${datadir}
+
+    echo "Starting Go dummy node."
+
+    ${WORKDIR}/ekiden-node \
+        --log.level debug \
+        --grpc.port 42261 \
+        --epochtime.backend tendermint_mock \
+        --beacon.backend insecure \
+        --storage.backend memory \
+        --scheduler.backend trivial \
+        --registry.backend tendermint \
+        --roothash.backend tendermint \
+        --tendermint.consensus.timeout_commit 250ms \
+        --datadir ${datadir} \
+        &> dummy-go.log &
+}
+
+run_compute_node() {
+    local id=$1
+    shift
+    local extra_args=$*
+
+    # Generate port number.
+    let "port=id + 10000"
+
+    echo "Starting compute node ${id} on port ${port}."
+
+    ekiden-compute \
+        --no-persist-identity \
+        --storage-backend multilayer \
+        --storage-multilayer-local-storage-base /tmp/ekiden-storage-persistent_${id} \
+        --storage-multilayer-bottom-backend remote \
+        --max-batch-timeout 100 \
+        --entity-ethereum-address 0000000000000000000000000000000000000000 \
+        --port ${port} \
+        ${extra_args} \
+        ${WORKDIR}/target/enclave/runtime-ethereum.so &> compute${id}.log &
+}
+
+run_gateway() {
+    local id=$1
+
+    # Generate port numbers.
+    let "http_port=id + 8544"
+    let "ws_port=id + 8554"
+    let "prometheus_port=id + 3000"
+
+    echo "Starting web3 gateway ${id} on ports ${http_port} and ${ws_port}."
+    target/debug/gateway \
+        --storage-backend multilayer \
+        --storage-multilayer-local-storage-base /tmp/ekiden-storage-persistent-gateway_${id} \
+        --storage-multilayer-bottom-backend remote \
+        --mr-enclave $(cat $WORKDIR/target/enclave/runtime-ethereum.mrenclave) \
+        --http-port ${http_port} \
+        --threads 100 \
+        --ws-port ${ws_port} \
+        --prometheus-metrics-addr 0.0.0.0:${prometheus_port} \
+        --prometheus-mode pull &> gateway${id}.log &
+}
+
 run_test() {
+    local dummy_node_runner=$1
+
     # Ensure cleanup on exit.
     trap 'kill -- -0' EXIT
 
@@ -74,18 +139,18 @@ run_test() {
     # snapshot manager can recover after initially failing to connect to the
     # root hash stream, and 2) whether the gateway waits for the committee to be
     # elected and connects to the leader.
-    bash ${WORKDIR}/scripts/utils.sh run_gateway 1
-    bash ${WORKDIR}/scripts/utils.sh run_gateway 2
+    run_gateway 1
+    run_gateway 2
     sleep 3
 
     # Start dummy node.
-    bash ${WORKDIR}/scripts/utils.sh run_dummy_node_go_tm
+    $dummy_node_runner
     sleep 1
 
     # Start compute nodes.
-    bash ${WORKDIR}/scripts/utils.sh run_compute_node 1
+    run_compute_node 1
     sleep 1
-    bash ${WORKDIR}/scripts/utils.sh run_compute_node 2
+    run_compute_node 2
 
     # Advance epoch to elect a new committee.
     sleep 3
