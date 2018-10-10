69c0a5c7be08cd4dedd3adba820ec47eefcbbf1e
Peter Gilbert <pgilbert@oasislabs.com>
2018-10-10 15:41:18 +0000

    Kill pub/sub notifier task on shutdown

--- gateway/src/notifier.rs
+++ gateway/src/notifier.rs
@@ -4,6 +4,7 @@ use std::process::abort;
 use std::sync::Arc;
 use std::time::Duration;
 
+use ekiden_common::futures::{killable, KillHandle};
 use ekiden_common::tokio::timer::Interval;
 use ekiden_core::environment::Environment;
 use ekiden_core::futures::FutureExt;
@@ -15,22 +16,24 @@ pub struct PubSubNotifier {
     client: Arc<Client>,
     environment: Arc<Environment>,
     interval_secs: u64,
+    notifier_task: Option<KillHandle>,
 }
 
 impl PubSubNotifier {
     pub fn new(client: Arc<Client>, environment: Arc<Environment>, interval_secs: u64) -> Self {
-        let instance = Self {
+        let mut instance = Self {
             client: client.clone(),
             environment: environment.clone(),
             interval_secs,
+            notifier_task: None,
         };
         instance.start();
         instance
     }
 
-    fn start(&self) {
+    fn start(&mut self) {
         let interval = Interval::new_interval(Duration::new(self.interval_secs, 0));
-        self.environment.spawn({
+        let (f, kill_handle) = killable({
             let client = self.client.clone();
             interval
                 .for_each(move |_| {
@@ -41,7 +44,17 @@ impl PubSubNotifier {
                     error!("Pub/sub notifier error: {}", e);
                     abort();
                 })
-                .into_box()
         });
+        self.environment.spawn(f.map(|_| ()).into_box());
+        self.notifier_task = Some(kill_handle);
+    }
+}
+
+impl Drop for PubSubNotifier {
+    fn drop(&mut self) {
+        // Kill the notifier task.
+        if let Some(task) = self.notifier_task.take() {
+            task.kill();
+        }
     }
 }
