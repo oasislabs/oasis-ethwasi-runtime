849fefebfcc514ff523e6ff4fff9a7c5f0b5b0fe
david-yan <david.yan@berkeley.edu>
2018-10-12 17:36:20 -0700

    Reformat tests to reuse code (#285)
    * Reformat tests to reuse code
    
    * Fix compute node command
    
    * Update call_contract interface

--- tests/deploy_contract/call_contract.js
+++ tests/deploy_contract/call_contract.js
@@ -5,14 +5,12 @@ let program = require('commander');
 let Web3 = require('web3');
 let Tx = require('ethereumjs-tx');
 
-program
-  .option('--contract-address <address>', 'Contract address', '0x00000000000000000000000000000000000000')
-  .parse(process.argv);
+program.parse(process.argv);
 
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 web3.eth.defaultAccount = '0x1cca28600d7491365520b31b466f88647b9839ec';
 
-web3.eth.call({to: program.contractAddress,}).then(result => {
+web3.eth.call({to: program.args[0],}).then(result => {
     console.log("called contract");
     console.log(result);
     process.exit();
