8dd34a19c4d337298ab84b2a439040b826e00fb6
Armani Ferrante <armaniferrante@gmail.com>
2018-10-27 18:11:28 +0000

    Estimate gas test reproducing issue

--- tests/test/test-confidential.js
+++ tests/test/test-confidential.js
@@ -8,14 +8,14 @@ const utils = require("./utils");
 contract("Confidential Contracts", async (accounts) => {
 
   const provider = utils.provider();
+  const artifact = utils.readArtifact("Counter");
+  const counterContract = new (new _web3()).eth.Contract(artifact.abi);
+  const initcode = counterContract.deploy({data: artifact.bytecode}).encodeABI();
+  const confidentialInitcode = utils.makeConfidential(initcode);
 
   it("stores the long term public key in the deploy logs", async () => {
-	const artifact = utils.readArtifact("Counter");
-	const counterContract = new (new _web3()).eth.Contract(artifact.abi);
-	const initcode = counterContract.deploy({data: artifact.bytecode}).encodeABI();
-	const deployData = utils.makeConfidential(initcode);
 	const deployTx = new Tx({
-	  data: deployData,
+	  data: confidentialInitcode,
 	  gasLimit: utils.GAS_LIMIT,
 	  from: provider.address,
 	  value: 0,
@@ -33,4 +33,22 @@ contract("Confidential Contracts", async (accounts) => {
 	assert.equal(log.data, "0x9385b8391e06d67c3de1675a58cffc3ad16bcf7cc56ab35d7db1fc03fb227a54");
 	assert.equal(log.logIndex, "0x0");
   });
+
+  it("should estimate gas for confidential and non confidential deploys the same", async () => {
+	const params = {
+	  from: provider.address,
+	  gasPrice: utils.GAS_PRICE,
+	  value: "0x00",
+	  nonce: (await utils.fetchNonce(provider.address)).result,
+	  data: confidentialInitcode
+	};
+	const confidentialGasUsed = await utils.makeRpc("eth_estimateGas", [params, "latest"]);
+	params["data"] = initcode;
+	const gasUsed = await utils.makeRpc("eth_estimateGas", [params, "latest"]);
+
+	console.log("conf = ", confidentialGasUsed);
+	console.log("Gas used = ", gasUsed);
+	assert.equal(confidentialGasUsed.result, gasUsed.result);
+  });
+
 });
