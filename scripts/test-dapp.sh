72446fc977a140aa6d83b897d3e8c0ec30bc9ae1
Armani Ferrante <armaniferrante@gmail.com>
2018-10-30 15:04:00 -0700

    Add celer test suite (#342)

--- scripts/test-dapp.sh
+++ scripts/test-dapp.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 
-# Runs the test suites for various popular dapps against our gateway.
-
-WORKDIR=${1:-$(pwd)}
+# Runs the test suites for dapps against our gateway.
+# CLI args: "celer" or "ens".
 
 source scripts/utils.sh
+
+WORKDIR=$(pwd)
 
 run_test() {
     # Ensure cleanup on exit.
@@ -26,14 +27,21 @@ run_test() {
     mkdir -p /tmp/dapps
     cd /tmp/dapps
 
-    run_dapps
+    run_dapp $1
 
     pkill -P $$
 }
 
-run_dapps() {
-    #run_ens
-    :
+run_dapp() {
+    case "$1" in
+        "celer")
+            run_celer
+            ;;
+        "ens")
+            #run_ens
+            :
+            ;;
+    esac
 }
 
 run_ens() {
@@ -42,14 +50,30 @@ run_ens() {
     git checkout ekiden
     npm install > /dev/null
     truffle test --network oasis_test & test_pid=$!
+    test_wait $test_pid
+    cd ../
+}
 
+run_celer() {
+    git clone https://github.com/oasislabs/cChannel-eth.git
+    cd cChannel-eth
+    git checkout ekiden
+    npm install > /dev/null
+    truffle compile > /dev/null
+    truffle migrate --network oasis_test
+    truffle test --network oasis_test & test_pid=$!
+    test_wait $test_pid
+    cd ../
+}
+
+test_wait() {
+    local test_pid=$1
     wait $test_pid
     test_ret=$?
     if [ $test_ret -ne 0 ]; then
         echo "ens test suite failed"
         exit $test_ret
     fi
-    cd ../
 }
 
-run_test
+run_test $1
