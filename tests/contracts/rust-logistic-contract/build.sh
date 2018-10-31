154409b785da47e6bf9274a0a14af584eee2eaeb
david-yan <david.yan@berkeley.edu>
2018-10-31 15:12:14 -0700

    Update wasm-build flag (#368)

--- tests/contracts/rust-logistic-contract/build.sh
+++ tests/contracts/rust-logistic-contract/build.sh
@@ -1,2 +1,2 @@
 cargo build --target wasm32-unknown-unknown --release
-wasm-build --target wasm32-unknown-unknown --stack-size 262144 ../../../target rust_logistic_contract
+wasm-build --target wasm32-unknown-unknown --max-mem 262144 ../../../target rust_logistic_contract
