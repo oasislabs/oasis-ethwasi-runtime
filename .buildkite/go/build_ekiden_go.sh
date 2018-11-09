880b39257036adb776bd5c06d5b1faebff52be23
Conor Gilsenan <conorgilsenan@gmail.com>
2018-11-08 22:51:12 -0500

    Update build for ekiden-node
    See commits:
    c689a546505ffd502a3644364ae9eb9a826fc2ea
    4875cdc5cd458606b82ae30468142d8f489e05df
    c95c719bed663bb32721959b0ecc17bf99fd9fb8

--- .buildkite/go/build_ekiden_go.sh
+++ .buildkite/go/build_ekiden_go.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-##
-# This script builds the go stuff...
+################################################################
+# This script builds ekiden-node.
 # 
 # Usage:
 # build_rust_runtime.sh [output_bin_path]
@@ -10,7 +10,7 @@
 #                   binary should be moved to. Useful for moving
 #                   the binary to a volume mounted to the docker
 #                   host.
-##
+################################################################
 
 # Helpful tips on writing build scripts:
 # https://buildkite.com/docs/pipelines/writing-build-scripts
@@ -18,16 +18,20 @@ set -euxo pipefail
 
 output_bin_path=${1:-NOT_DEFINED}
 
+##################
 # Install protobuf
+##################
 # TODO Check whether this actually needs to happen.
 #      Something similar is already in the base image.
 go get -d github.com/golang/protobuf/protoc-gen-go
 cd $GOPATH/src/github.com/golang/protobuf
-git checkout v1.0.0
+git checkout v1.1.0
 cd protoc-gen-go
 go install
 
+############################
 # Download oasis/ekiden repo
+############################
 # TODO This binary should really be built in the
 #      pipeline for the oasislabs/ekiden repo and
 #      exposed as an artifact. There is no need to
@@ -41,11 +45,14 @@ git clone --depth 1 git@github.com:/oasislabs/ekiden
 
 # Build ekiden/go
 cd ekiden/go
-dep ensure
-go generate ./...
-go build -v -o ./ekiden/ekiden ./ekiden
-cd $GOPATH/src/github.com/oasislabs/ekiden/go/ekiden
-go install
+env -u GOPATH make
+cp \
+  $GOPATH/src/github.com/oasislabs/ekiden/go/ekiden/ekiden \
+  /go/bin
+
+cd benchmark
+env -u GOPATH make
+cp benchmark /go/bin
 
 if [ $output_bin_path != "NOT_DEFINED" ]; then
   mv /go/bin/ekiden $output_bin_path
