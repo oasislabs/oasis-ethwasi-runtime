5fb5fb796133149384d677c9b95d82c652f871a6
sharmaster96 <ennsharma@oasislabs.com>
2018-11-13 22:35:53 +0000

    Fixed style issues for CircleCI

--- tests/contracts/tvm_contract/build.rs
+++ tests/contracts/tvm_contract/build.rs
@@ -1,4 +1,4 @@
 fn main() {
-  // Required step to link the TVM module.
-  println!("cargo:rustc-link-search=native={}", "tvm_module");
+    // Required step to link the TVM module.
+    println!("cargo:rustc-link-search=native={}", "tvm_module");
 }
