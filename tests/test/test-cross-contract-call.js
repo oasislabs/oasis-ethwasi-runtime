a076ed531abee004867ebcbc9d8e43a7b2452881
armaniferrante <armaniferrante@gmail.com>
2019-01-10 00:40:20 +0000

    Spaces

--- tests/test/test-cross-contract-call.js
+++ tests/test/test-cross-contract-call.js
@@ -9,41 +9,41 @@ const web3 = new Web3(Deployed.web3.currentProvider);
 contract("CrossContractCall", (accounts) => {
 
   let testCases = [
-	[Deployed, Existing, "should update value in other solidity contract"],
-	[DeployedRust, ExistingRust, "should update value in other rust contract"]
+    [Deployed, Existing, "should update value in other solidity contract"],
+    [DeployedRust, ExistingRust, "should update value in other rust contract"]
   ];
 
   testCases.forEach((test) => {
-	it(test[2], async () => {
-	  let deployedArtifact = test[0];
-	  let existingArtifact = test[1];
+    it(test[2], async () => {
+      let deployedArtifact = test[0];
+      let existingArtifact = test[1];
 
-	  const deployedContract = new web3.eth.Contract(deployedArtifact.abi);
-	  let deployInstance = await deployedContract.deploy({
-		data: deployedArtifact.bytecode
-	  }).send({
-		from: accounts[0]
-	  });
+      const deployedContract = new web3.eth.Contract(deployedArtifact.abi);
+      let deployInstance = await deployedContract.deploy({
+        data: deployedArtifact.bytecode
+      }).send({
+        from: accounts[0]
+      });
 
-	  let prevA = await deployInstance.methods.a().call();
-	  assert.equal(prevA, 1, "Previous value is incorrect");
+      let prevA = await deployInstance.methods.a().call();
+      assert.equal(prevA, 1, "Previous value is incorrect");
 
-	  const existingContract = new web3.eth.Contract(existingArtifact.abi, undefined, {
-		from: accounts[0]
-	  });
-	  let existingInstance = await existingContract.deploy({
-		data: existingArtifact.bytecode,
-		arguments: [deployInstance.options.address]
-	  }).send();
+      const existingContract = new web3.eth.Contract(existingArtifact.abi, undefined, {
+        from: accounts[0]
+      });
+      let existingInstance = await existingContract.deploy({
+        data: existingArtifact.bytecode,
+        arguments: [deployInstance.options.address]
+      }).send();
 
-	  prevA = await existingInstance.methods.get_a().call();
-	  assert.equal(prevA, 1, "Previous value is incorrect");
+      prevA = await existingInstance.methods.get_a().call();
+      assert.equal(prevA, 1, "Previous value is incorrect");
 
-	  // remove gas param oncce this issue is addressed
-	  // https://github.com/oasislabs/runtime-ethereum/issues/478
-	  await existingInstance.methods.set_a(2).send({gas: '0x100000'});
-	  let newA = await deployInstance.methods.a().call();
+      // remove gas param oncce this issue is addressed
+      // https://github.com/oasislabs/runtime-ethereum/issues/478
+      await existingInstance.methods.set_a(2).send({gas: '0x100000'});
+      let newA = await deployInstance.methods.a().call();
       assert.equal(newA, 2, "Contract value was not updated")
-	});
+    });
   });
 });
