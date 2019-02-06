3316f293674423f43d53dcb3f0ff051ff8320015
Armani Ferrante <armaniferrante@gmail.com>
2019-02-05 16:54:51 -0800

    ci: Add tendermint.debug.addr_book_lenient to ekiden node (#552)

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -103,6 +103,7 @@ run_backend_tendermint_committee() {
             --tendermint.core.genesis_file ${genesis_file} \
             --tendermint.core.listen_address tcp://0.0.0.0:${tm_port} \
             --tendermint.consensus.timeout_commit 250ms \
+            --tendermint.debug.addr_book_lenient \
             --tendermint.log.debug \
             --datadir ${datadir} \
             &
@@ -158,6 +159,7 @@ run_compute_node() {
         --tendermint.core.genesis_file ${EKIDEN_TM_GENESIS_FILE} \
         --tendermint.core.listen_address tcp://0.0.0.0:${tm_port} \
         --tendermint.consensus.timeout_commit 250ms \
+        --tendermint.debug.addr_book_lenient \
         --tendermint.log.debug \
         --worker.backend sandboxed \
         --worker.binary ${EKIDEN_WORKER} \
