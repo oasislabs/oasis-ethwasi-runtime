58b59f25aa9ca8774f061748de91352733dc8484
Warren He <w@oasislabs.com>
2019-02-15 16:03:15 -0800

    docker: all-in-one gateway image

---
+++ docker/all-in-one/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+docker run \
+    --detach \
+    --rm \
+    --name test \
+    --security-opt=apparmor=unconfined \
+    --security-opt=seccomp=unconfined \
+    --volume="$PWD/../private-ops/untracked/ias-dev-creds:/mnt/ias-creds" \
+    --device=/dev/isgx \
+    --publish=127.0.0.1:8545:8545/tcp \
+    --publish=127.0.0.1:8555:8555/tcp \
+    oasislabs/gateway-all-in-one:staging-hw
