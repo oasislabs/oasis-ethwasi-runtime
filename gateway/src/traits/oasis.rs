998880fece0f7b8d8e4b1e455575bb3fc8048492
Peter Gilbert <pgilbert@oasislabs.com>
2019-02-18 21:20:11 +0000

    gateway: move confidential apis to oasis

--- gateway/src/traits/oasis.rs
+++ gateway/src/traits/oasis.rs
@@ -1,16 +1,42 @@
 //! Oasis rpc interface.
 
-use jsonrpc_core::BoxFuture;
+use ethereum_types::Address;
+use jsonrpc_core::{BoxFuture, Result};
 use jsonrpc_macros::Trailing;
 
-use parity_rpc::v1::types::{BlockNumber, H160};
+use parity_rpc::v1::types::{BlockNumber, Bytes, CallRequest, H160};
 
 build_rpc_trait! {
     pub trait Oasis {
+        type Metadata;
+        /// Returns the public key of a contract, given its address.
+        #[rpc(name = "oasis_getPublicKey")]
+        fn public_key(&self, Address) -> Result<PublicKeyResult>;
+        /// Executes a new message call without creating a transaction on chain.
+        /// Returns the return value of the executed contract, encrypted with
+        /// the user's public key.
+        #[rpc(meta, name = "oasis_call_enc")]
+        fn call_enc(
+            &self,
+            Self::Metadata,
+            CallRequest,
+            Trailing<BlockNumber>
+        ) -> BoxFuture<Bytes>;
         /// Get expiration timestamp for a contract.
         /// The value is a Unix timestamp (seconds since the epoch). A special value of 0 indicates
         /// that the contract's storage does not expire.
         #[rpc(name = "oasis_getExpiry")]
         fn get_expiry(&self, H160, Trailing<BlockNumber>) -> BoxFuture<u64>;
     }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PublicKeyResult {
+    /// Public key of the contract
+    pub public_key: Bytes,
+    /// Time at which the key expires.
+    pub timestamp: u64,
+    /// Signature from the key manager authenticating the public key,
+    /// i.e., Sign(ssk, (pk, t).
+    pub signature: Bytes,
 }
