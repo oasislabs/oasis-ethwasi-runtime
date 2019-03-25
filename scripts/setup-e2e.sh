7a3cc41167e4e2ffcc75591af89c0f1ead0fd282
Peter Gilbert <petergilbert@gmail.com>
2019-03-25 10:36:31 -0700

    Remove some unneeded installs from setup-e2e.sh (#662)

--- scripts/setup-e2e.sh
+++ scripts/setup-e2e.sh
@@ -14,12 +14,6 @@ pushd /e2e-tests/ > /dev/null
 npm install
 popd > /dev/null
 
-echo "Installing wasm32-unknown-unknown target."
-rustup target add wasm32-unknown-unknown
-
-echo "Installing wscat."
-npm install -g wscat
-
 echo "Installing jq."
 apt-get install -y jq
 
