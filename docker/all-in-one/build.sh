1abb209c14f6ee1def6509a4f91dd4d77d6f74f8
Warren He <w@oasislabs.com>
2019-04-26 15:35:16 -0700

    docker: force pull all-in-one base image

--- docker/all-in-one/build.sh
+++ docker/all-in-one/build.sh
@@ -5,6 +5,7 @@ tar -c \
     docker/all-in-one/service \
     docker/all-in-one/Dockerfile \
 | docker build \
+    --pull \
     --file=docker/all-in-one/Dockerfile \
     --tag=local-aio:latest \
     -
