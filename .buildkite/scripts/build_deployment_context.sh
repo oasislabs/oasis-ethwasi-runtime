2993b44bfe1434821fbcf8aa5be0c2deb9917644
Peter Gilbert <pgilbert@oasislabs.com>
2019-06-27 22:40:38 +0000

    Build benchmarking images

--- .buildkite/scripts/build_deployment_context.sh
+++ .buildkite/scripts/build_deployment_context.sh
@@ -11,9 +11,19 @@ set -euxo pipefail
 
 platform_dir=$1
 context=$2
+if [ -n "${BUILD_PRODUCTION_GENESIS:-}" -a -n "${BUILD_BENCHMARKING:-}" ]; then
+    echo "Cannot use BUILD_PRODUCTION_GENESIS and BUILD_BENCHMARKING options together!"
+    exit 1
+fi
+
 if [ -n "${BUILD_PRODUCTION_GENESIS:-}" ]; then
     export RUNTIME_BUILD_EXTRA_ARGS='--features production-genesis'
     export GATEWAY_BUILD_EXTRA_ARGS='--features production-genesis'
+fi
+
+if [ -n "${BUILD_BENCHMARKING:-}" ]; then
+    export RUNTIME_BUILD_EXTRA_ARGS='--features benchmarking'
+    export GATEWAY_BUILD_EXTRA_ARGS='--features benchmarking'
 fi
 
 buildkite-agent artifact download "$platform_dir/*" .
