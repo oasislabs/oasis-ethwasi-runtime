663392c90746f47756190a6ea757d5b71e52e2f5
Armani Ferrante <armaniferrante@gmail.com>
2019-07-01 15:35:10 -0700

    Simplify example

--- tests/contracts/mantle-counter/src/main.rs
+++ tests/contracts/mantle-counter/src/main.rs
@@ -1,38 +1,17 @@
-#[macro_use]
-extern crate serde;
+use mantle::{Context, Service};
 
-use mantle::{Address, Context, Event, Service};
-
-pub type Result<T> = std::result::Result<T, Error>;
-
-#[derive(Debug, Eq, PartialEq, failure::Fail)]
-pub enum Error {
-    #[fail(display = "Unknown error occured.")]
-    Unknown,
-}
-
-#[derive(Service, Default)]
+#[derive(Service)]
 pub struct MantleCounter {
     count: u64,
 }
 
 impl MantleCounter {
-
-    pub fn new(ctx: &Context) -> Result<Self> {
+    pub fn new(_ctx: &Context) -> Result<Self, String> {
         Ok(Self {
             count: 0
         })
     }
-
-    pub fn get_count(&mut self, ctx: &Context) -> Result<u64> {
-        Ok(self.count)
-    }
-
-    pub fn increment_count(&mut self, ctx: &Context) -> Result<()> {
-        Ok(())
-    }
 }
-
 
 fn main() {
     mantle::service!(MantleCounter);
