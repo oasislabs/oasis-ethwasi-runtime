8a13521a6540288b2abd4a6e59cc0cc8045bf804
Peter Gilbert <pgilbert@oasislabs.com>
2020-04-17 03:46:19 +0000

    Fix a compiler warning

--- bin/main.rs
+++ bin/main.rs
@@ -16,10 +16,8 @@ use std::sync::Arc;
 use serde_bytes::ByteBuf;
 
 use oasis_core_runtime::{
-    common::{runtime::RuntimeId, version::Version},
-    rak::RAK,
-    register_runtime_txn_methods, version_from_cargo, Protocol, RpcDemux, RpcDispatcher,
-    TxnDispatcher, TxnMethDispatcher,
+    common::version::Version, rak::RAK, register_runtime_txn_methods, version_from_cargo, Protocol,
+    RpcDemux, RpcDispatcher, TxnDispatcher, TxnMethDispatcher,
 };
 use oasis_runtime::block::OasisBatchHandler;
 use oasis_runtime_api::{with_api, ExecutionResult};
