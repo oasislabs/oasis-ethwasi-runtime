978eb67934488cfedd3e19b57e679c424186ac48
Mitja T <mitjat@oasislabs.com>
2020-06-11 19:12:34 +0200

    [tiny] More informative error msg in confidential_ctx.rs

--- common/src/confidential/confidential_ctx.rs
+++ common/src/confidential/confidential_ctx.rs
@@ -188,10 +188,20 @@ impl EthConfidentialCtx for ConfidentialCtx {
     }
 
     fn encrypt_session(&mut self, data: Vec<u8>) -> Result<Vec<u8>> {
-        if self.peer_public_key.is_none() || self.contract.is_none() || self.next_nonce.is_none() {
-            return Err(Error::Confidential(
-                "must have key pair of a contract and peer and a next nonce".to_string(),
-            ));
+        if self.peer_public_key.is_none() {
+            err_msg.push_str("; no peer public key specified");
+        }
+        if self.contract.is_none() {
+            err_msg.push_str("; no contract specified from which to extract contract public key");
+        }
+        if self.next_nonce.is_none() {
+            err_msg.push_str("; no next nonce specified");
+        }
+        if !err_msg.is_empty() {
+            return Err(Error::Confidential(format!(
+                "Cannot encrypt session {}.",
+                err_msg
+            )));
         }
 
         let contract_key = &self.contract.as_ref().unwrap().1;
@@ -221,7 +231,7 @@ impl EthConfidentialCtx for ConfidentialCtx {
         let contract_secret_key = self.contract.as_ref().unwrap().1.input_keypair.get_sk();
 
         let decryption = crypto::decrypt(Some(encrypted_payload), contract_secret_key)
-            .map_err(|err| Error::Confidential(err.to_string()))?;
+            .map_err(|err| Error::Confidential(format!("Failed to decrypt session payload ({} bytes): {}", encrypted_payload.size(), err.to_string())))?;
         self.peer_public_key = Some(decryption.peer_public_key);
 
         let mut nonce = decryption.nonce;
